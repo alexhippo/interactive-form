508556873857ccf177a697ccc9c37a9ee56fec78
Alex Hipolito <alexheepo@gmail.com>
2021-04-20 22:52:45 +1000

    Focus on name field when the page first loads

---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+* Treehouse Techdegree:
+* FSJS Project 3 - Interactive Form
+* Author: Alex Hipolito
+* GitHub: @alexhippo
+*/
+
+//Initial focus on name field when you load the page
+document.getElementById('name').focus();
