b985b8469d359231a8e866272313d202b091aa5b
Alex Hipolito <alexheepo@gmail.com>
2021-04-21 22:31:01 +1000

    Ensure Other Job Role field is hidden on page load and is visible if user selects 'Other' job role from title selection dropdown

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@
 
 //Initial focus on name field when you load the page
 document.getElementById('name').focus();
+
+//Job Role field
+//Other Job Role field should be hidden by default
+const otherJobRole = document.getElementById('other-job-role');
+//@to-do: Ensure this is hidden from the start
+otherJobRole.style.visibility = 'hidden';
+document.getElementById('title').addEventListener('change', (event) => {
+    if (event.target.value === 'other') {
+        otherJobRole.style.visibility = 'visible';
+    } else {
+        otherJobRole.style.visbility = 'hidden';
+    }
+});
