1f5d0ccc628789426fbc1832a7193ded253d3c94
Alex Hipolito <alexheepo@gmail.com>
2021-04-21 23:38:01 +1000

    T-shirt Color field should be disabled until user chooses a Tshirt Design. Only display colours in T-Shirt Color field that apply to the chosen T-Shirt design

--- js/script.js
+++ js/script.js
@@ -9,14 +9,34 @@
 document.getElementById('name').focus();
 
 //Job Role field
-//Other Job Role field should be hidden by default
 const otherJobRole = document.getElementById('other-job-role');
+const title = document.getElementById('title');
 //@to-do: Ensure this is hidden from the start
 otherJobRole.style.visibility = 'hidden';
-document.getElementById('title').addEventListener('change', (event) => {
+
+title.addEventListener('change', (event) => {
     if (event.target.value === 'other') {
         otherJobRole.style.visibility = 'visible';
     } else {
         otherJobRole.style.visbility = 'hidden';
     }
 });
+
+//T-Shirt Info - Color and Design
+const tshirtColor = document.getElementById('color');
+const tshirtDesign = document.getElementById('design');
+tshirtColor.setAttribute('disabled', 'true');
+
+tshirtDesign.addEventListener('change', (event) => {
+    tshirtColor.removeAttribute('disabled');
+    tshirtColor.querySelectorAll('option');
+    for (let i = 0; i < tshirtColor.length; i++) {
+        tshirtColor[i].hidden = true;
+    };
+
+    const chosenDesign = event.target.value;
+    const tshirtColors = tshirtColor.querySelectorAll(`option[data-theme="${chosenDesign}"]`);
+    for (let i = 0; i < tshirtColors.length; i++) {
+        tshirtColors[i].hidden = false;
+    };
+})
